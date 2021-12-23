52d674b1be7fc13e77b8ae0cab4f2f23a2751d8b
Percy <percy@railway.app>
2021-12-23 13:57:05 +0000

    Initial Commit

---
+++ src/layouts/authors.ts
@@ -0,0 +1,30 @@
+export const authors = [
+  {
+    name: "Faraz Patankar",
+    image: "https://og.railway.app/authors/faraz-patankar.jpeg",
+  },
+  {
+    name: "Jake Cooper",
+    image: "https://og.railway.app/authors/jake-cooper.jpeg",
+  },
+  {
+    name: "Greg Schier",
+    image: "https://og.railway.app/authors/greg-schier.jpeg",
+  },
+  {
+    name: "Jake Runzer",
+    image: "https://og.railway.app/authors/jake-runzer.jpeg",
+  },
+  {
+    name: "Angelo Saraceno",
+    image: "https://og.railway.app/authors/angelo-saraceno.jpeg",
+  }
+];
+
+const defaultAuthor = authors[0];
+
+export const getAuthor = (name: string) => {
+  const author = authors.find(author => author.name === name);
+
+  return author ?? defaultAuthor;
+};
